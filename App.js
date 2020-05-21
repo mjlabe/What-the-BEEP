b72df1e2566622df0bb523775a014207c6cb4a07
Marc LaBelle <mlabelle@acvauctions.com>
2020-05-21 13:43:44 -0400

    move to react navigation

--- App.js
+++ App.js
@@ -1,66 +1,101 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-// import 'react-native-gesture-handler';
+import * as React from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  StatusBar,
-  Image,
-  Dimensions,
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    Dimensions,
+    StatusBar,
+    SafeAreaView,
+    Button,
 } from 'react-native';
-
-// import {NavigationContainer} from '@react-navigation/native';
-
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//     </NavigationContainer>
-//   );
-// }
-
-import BeepHeader from './main/beepHeader';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Colors from './main/Colors';
 
-const {width, height} = Dimensions.get('window');
+function HomeScreen() {
+    return (
+        <SafeAreaView style={styles.home}>
+            <StatusBar barStyle="light-content" backgroundColor="#6a51ae"/>
+            <View style={styles.body}>
+                <Image
+                    source={require('./main/images/dog_round.png')}
+                    alt=""
+                    style={styles.button}
+                />
+            </View>
+        </SafeAreaView>
+    );
+}
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <BeepHeader />
-        <View style={styles.body}>
-          <Image
-            source={require('./main/images/dog_round.png')}
-            alt=""
-            style={styles.button}
-          />
+function SettingsScreen() {
+    return (
+        <View style={styles.home}>
+            <Text>Details Screen</Text>
         </View>
-      </SafeAreaView>
-    </>
-  );
-};
+    );
+}
+
+let dimensions = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  body: {
-    backgroundColor: Colors.darker,
-    height: height,
-    width: width,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    // justifyContent: 'center',
-    // alignItems: 'center',
-  },
+    home: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: Colors.darker,
+    },
+    button: {
+        width: dimensions.width / 2,
+        height: dimensions.width / 2,
+    },
+    logo: {
+        marginLeft: 10,
+        height: dimensions.height/25 - 10,
+        width: dimensions.height/25 - 10,
+    },
 });
+
+const Stack = createStackNavigator();
+
+function App() {
+    return (
+        <NavigationContainer>
+            <StatusBar barStyle="dark-content"/>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{
+                        title: 'What the BEEP?!',
+                        headerStyle: {
+                            backgroundColor: Colors.black,
+                        },
+                        headerTintColor: '#fff',
+                        headerTitleStyle: {
+                            fontWeight: 'bold',
+                        },
+                        headerRight: () => (
+                            <Button
+                                onPress={() => alert('This is a button!')}
+                                title="Info"
+                                color="#fff"
+                            />
+                        ),
+                        headerLeft: () => (
+                            <Image
+                                source={require('./main/images/dog-icon-light.png')}
+                                alt=""
+                                resizeMode={'stretch'}
+                                style={styles.logo}
+                            />
+                        ),
+                    }}
+                />
+                <Stack.Screen name="Settings" component={SettingsScreen}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
 
 export default App;
