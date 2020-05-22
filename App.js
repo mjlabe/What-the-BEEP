a4671738357e72ca474b2ff72be8f08066b8ecea
Marc LaBelle <mlabelle@acvauctions.com>
2020-05-22 10:20:10 -0400

    add beep sound

--- App.js
+++ App.js
@@ -12,14 +12,30 @@ import {
 } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import Sound from 'react-native-sound';
+
 import Colors from './main/Colors';
 
-function HomeScreen() {
+function HomeScreen({navigation}) {
+    React.useLayoutEffect(() => {
+        navigation.setOptions({
+            headerLeft: () => (
+                <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+                    <Image
+                        source={require('./main/images/dog-icon-light.png')}
+                        alt=""
+                        resizeMode={'stretch'}
+                        style={styles.logo}
+                    />
+                </TouchableOpacity>
+            ),
+        });
+    });
     return (
         <SafeAreaView style={styles.home}>
-            <StatusBar barStyle="light-content" backgroundColor="#6a51ae"/>
+            <StatusBar barStyle="light-content" backgroundColor={Colors.black}/>
             <View style={styles.body}>
-                <TouchableOpacity onPress={() => alert('BEEP!')}>
+                <TouchableOpacity onPress={() => beep()}>
                     <Image
                         source={require('./main/images/dog_round.png')}
                         alt=""
@@ -31,11 +47,14 @@ function HomeScreen() {
     );
 }
 
-function SettingsScreen() {
+function SettingsScreen({navigation}) {
     return (
-        <View style={styles.home}>
-            <Text>Details Screen</Text>
-        </View>
+        <SafeAreaView style={styles.home}>
+            <StatusBar barStyle="light-content" backgroundColor={Colors.black}/>
+            <View style={styles.body}>
+                <Text>Details Screen</Text>
+            </View>
+        </SafeAreaView>
     );
 }
 
@@ -53,11 +72,26 @@ const styles = StyleSheet.create({
         height: dimensions.width / 2,
     },
     logo: {
-        marginLeft: 10,
+        marginLeft: 8,
         height: 30,
         width: 30,
     },
 });
+
+let url = './main/audio/beep.m4a';
+
+function beep() {
+    let sound1 = new Sound(require(url),
+        (error, sound) => {
+            if (error) {
+                alert('error' + error.message);
+                return;
+            }
+            sound1.play(() => {
+                sound1.release();
+            });
+        });
+}
 
 const Stack = createStackNavigator();
 
@@ -78,19 +112,22 @@ function App() {
                         headerTitleStyle: {
                             fontWeight: 'bold',
                         },
-                        headerLeft: () => (
-                            <TouchableOpacity onPress={() => alert('Settings!')}>
-                                <Image
-                                    source={require('./main/images/dog-icon-light.png')}
-                                    alt=""
-                                    resizeMode={'stretch'}
-                                    style={styles.logo}
-                                />
-                            </TouchableOpacity>
-                        ),
                     }}
                 />
-                <Stack.Screen name="Settings" component={SettingsScreen}/>
+                <Stack.Screen
+                    name="Settings"
+                    component={SettingsScreen}
+                    options={{
+                        headerStyle: {
+                            backgroundColor: Colors.black,
+                        },
+                        headerTintColor: '#fff',
+                        headerTitleStyle: {
+                            fontWeight: 'bold',
+                        },
+                        headerBackTitleVisible: false,
+                    }}
+                />
             </Stack.Navigator>
         </NavigationContainer>
     );
